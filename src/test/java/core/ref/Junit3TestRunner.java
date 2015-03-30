4094d1ff61be315fd86c30901589e83a1dba5a27
erin314 <erin314@naver.com>
2015-03-30 18:32:40 +0900

    과제 2 리펙토링

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -6,19 +6,21 @@ import org.junit.Test;
 
 public class Junit3TestRunner {
 	@Test
-	public void run() throws Exception {
-		Class<Junit3Test> clazz1 = Junit3Test.class;
-		Class<?> clazz = clazz1.getSuperclass();
+	public <ERIN> void run() throws Exception {
+		Class<Junit3Test> clazz = Junit3Test.class;
+//		Class<ERIN> clazz = (Class<ERIN>) clazz1.getSuperclass();
 		
 		Method[] meths = clazz.getDeclaredMethods();
 		Method[] meths2 = clazz.getMethods();
 		
 		System.out.println(meths.length);
 		for (Method method : meths) {
+			if (!method.getName().startsWith("test")) {
+				continue;
+			}
 			method.invoke(clazz.newInstance());
 		}
 		for (Method method : meths2) {
-			
 			System.out.println(method);
 		}
 
