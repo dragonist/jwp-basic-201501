3f3471aedd8bdf8eb334fa27e820e78bdd3dd307
erin314 <erin314@naver.com>
2015-03-30 12:24:35 +0900

    # 1차 완성

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,11 @@
 package core.ref;
 
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import next.model.Question;
 
 import org.junit.Test;
@@ -13,5 +19,30 @@ public class ReflectionTest {
 	public void showClass() {
 		Class<Question> clazz = Question.class;
 		logger.debug(clazz.getName());
+		
+
+		
+		
+		
+		logger.debug("******************************************field");
+		Field[] f = clazz.getDeclaredFields();
+		for (Field field : f) {
+			logger.debug(field.getName());
+		}
+		
+		
+		logger.debug("******************************************constructor");
+		Constructor<?>[] c = clazz.getConstructors();
+		for (Constructor<?> constructor : c) {
+			logger.debug(constructor.getName());
+		}
+		
+		
+		logger.debug("******************************************method");
+		Method[] m =clazz.getMethods();
+		for (Method method : m) {
+			logger.debug(method.getName());
+		}
 	}
+	
 }
